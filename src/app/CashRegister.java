6b6d45042414078e0a445bee42f96fe27f48d077
Liz <lizbaze4730@gmail.com>
2022-10-22 15:37:15 -0500

    Accounted for further double inaccuracies

--- src/app/CashRegister.java
+++ src/app/CashRegister.java
@@ -25,42 +25,34 @@ public class CashRegister {
 		System.out.println("Please enter the cash amount tendered: ");
 		double payment = rounder(sc.nextDouble());
 		double change = payment - total;
-		String changeGiven = "Amount: " + total + ". Tendered: " + payment + ". Change: ";
+		String changeGiven = "Amount: " + (total/100) + ". Tendered: " + (payment/100) + ". Change: ";
 
 		while (change > 0) {
 			rounder(change);
-			if (change / 20 >= 1) {
+			if (change / 2000 >= 1) {
 				twenties += 1;
-				change -= 20;
-				change = rounder(change);
-			} else if (change / 10 >= 1) {
+				change -= 2000;
+			} else if (change / 1000 >= 1) {
 				tens += 1;
-				change -= 10;
-				change = rounder(change);
-			} else if (change / 5 >= 1) {
+				change -= 1000;
+			} else if (change / 500 >= 1) {
 				fives += 1;
-				change -= 5;
-				change = rounder(change);
-			} else if (change / 1 >= 1) {
+				change -= 500;
+			} else if (change / 100 >= 1) {
 				ones++;
-				change -= 1;
-				change = rounder(change);
-			} else if (change / .25 >= 1) {
+				change -= 100;
+			} else if (change / 25 >= 1) {
 				quarters++;
-				change -= .25;
-				change = rounder(change);
-			} else if (change / .10 >= 1) {
+				change -= 25;
+			} else if (change / 10 >= 1) {
 				dimes++;
-				change -= .10;
-				change = rounder(change);
-			} else if (change / .05 >= 1) {
+				change -= 10;
+			} else if (change / 5 >= 1) {
 				nickels++;
-				change -= .05;
-				change = rounder(change);
-			} else if (change / .01 >= 1) {
+				change -= 5;
+			} else if (change / 1 >= 1) {
 				pennies++;
-				change -= .01;
-				change = rounder(change);
+				change -= 1;
 			}
 		}
 
@@ -69,7 +61,7 @@ public class CashRegister {
 		} else if (twenties == 1) {
 			changeGiven += "1 twenty dollar bill";
 		}
-		if (tens > 1 && twenties >= 1) { // FINISH ACCOUNTING FOR COMMAS
+		if (tens > 1 && twenties >= 1) { 
 			changeGiven += ", " + tens + " ten dollar bills";
 		} else if (tens > 1) {
 			changeGiven += tens + "ten dollar bills";
@@ -146,8 +138,9 @@ public class CashRegister {
 		sc.close();
 	}
 
-	public static double rounder(double x) { // Rounds double to two decimals
-		x = Math.round(x * 100.0) / 100.0;
-		return x;
+	public static int rounder(double x) { // Rounds double to two decimals
+		x *= 100;
+		int y = (int) x;
+		return y;
 	}
 }
